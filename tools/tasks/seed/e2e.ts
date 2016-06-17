ffe0e550f298235ed35993e05e5cca9b27d179ef
harman deol <punzabi@gmail.com>
2016-06-17 10:19:54 +1000

    proper fix for https://github.com/mgechev/angular2-seed/issues/970

--- tools/tasks/seed/e2e.ts
+++ tools/tasks/seed/e2e.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
 import * as gulp from 'gulp';
+import { resolve } from 'path';
 import { protractor } from 'gulp-protractor';
+import * as fallback from 'express-history-api-fallback';
 
 class Protractor {
   server(port: number, dir: string) {
+    let root = resolve(process.cwd(), dir);
     let app = express();
-    app.use(express.static(dir));
-    app.all('*', function(req, res) {
-      res.sendfile(dir + '/index.html');
-    });
+    app.use(express.static(root));
+    app.use(fallback('index.html', { root }));
     return new Promise((resolve, reject) => {
       let server = app.listen(port, () => {
         resolve(server);
